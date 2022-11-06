a206c4362dbb92bf5037f4c5f6f238dd4bbd3749
NikSevenEleven <kursdz@mail.ru>
2022-11-06 21:57:51 +0300

    read_one

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -38,14 +38,7 @@
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-
-        <!-- Sidebar -->
-        <div class="sidebar">
-
-        </div>
-        <!-- /.sidebar -->
-    </aside>
+    @include('admin.includes.sidebar')
     @yield('content')
 
     <footer class="main-footer">
